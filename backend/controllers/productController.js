4a97bdf407688876b76e5ed942239ec8ec52b3bc
Zunaid-jony <jony35666666@gmail.com>
2022-06-19 18:58:07 +0600

    adin product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,3 +18,24 @@ exports.getAllProducts = async (req, res) =>{
     
     })
 }
+
+//update product
+exports.updateProduct = async (req,res,next)=>{
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success:true,
+            massage:"Product not found"
+
+        })
+      
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new:true, runValidators:true, useFindAndModify:false
+    })
+    res.status(200).json({
+        success:true,
+        product
+
+    })
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,15 @@
 const express = require("express")
-const { getAllProducts,createProduct } = require("../controllers/productController")
+const { getAllProducts,createProduct, updateProduct } = require("../controllers/productController")
 const router = express.Router()
 
 router.route("/products").get(getAllProducts)
 
 //product modal
 router.route("/product/new").post(createProduct)
+
+
+
+router.route("/product/:id").put(updateProduct)
 
 
  module.exports = router
